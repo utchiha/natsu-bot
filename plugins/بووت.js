2f203918f3b3892afc891172394e4967490589a6
utchiha <167469665+utchiha@users.noreply.github.com>
2024-04-20 22:05:21 +0200

    Update بووت.js

--- plugins/بووت.js
+++ plugins/بووت.js
@@ -2,9 +2,9 @@ let handler = async (m, { conn }) => {
     let user = global.db.data.users[m.sender];
     let name = conn.getName(m.sender);
     let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
-    let message = `مرحبا معك بوت ناتسو اذا اردت شي اكتب (.مهام)`;
+    let message = `مرحبا معك بوت يوريتشي اذا اردت شي اكتب (.مهام)`;
 
-    conn.sendFile(m.chat, 'https://telegra.ph/file/82e0c0e8451ec05e854f3.jpg', 'image.jpg', message, m);
+    conn.sendFile(m.chat, 'https://telegra.ph/file/150e05041d229b5522b0d.jpg', 'image.jpg', message, m);
 };
 
 handler.customPrefix = /^(bot|بوت|بووت|بوت)$/i;
