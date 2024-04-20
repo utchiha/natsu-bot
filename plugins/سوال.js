ea448b1404886d9998129c4571fbb7c14e7fcfe1
utchiha <167469665+utchiha@users.noreply.github.com>
2024-04-20 21:28:27 +0200

    Update سوال.js

--- plugins/سوال.js
+++ plugins/سوال.js
@@ -19,7 +19,7 @@ let handler = async (m, { conn, usedPrefix }) => {
 
 *الـوقـت⏳↞ ${(timeout / 1000).toFixed(2)}*
 *الـجـائـزة💰↞ ${poin} نقاط*
-*𝑁𝐴𝑇𝑺𝑈 BOT*
+*𝐘𝐎𝐑𝐈𝐂𝐇𝐈 𝐁𝐎𝐓*
 `.trim()
     conn.tekateki[id] = [
        await conn.reply(m.chat, caption, m),
